1ce233c8f76040d5f674f79d0129412cf3a960fd
Aleksey Lovchikov <alelov@brabdev.com>
2017-09-14 14:12:19 +0300

    add strict mode

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const request = require('request');
 const cheerio = require('cheerio');
 const https = require('https');
@@ -13,56 +15,61 @@ console.log('### this is alpha version ###'.toUpperCase().bgWhite.black);
 console.log('#############################\n'.bgWhite.black);
 
 function getVideos(url) {
-    return new Promise(function(resolve, reject) {
-        let result = [];
-        let names = [];
-        request(url, function(err, res, html) {
-            if(!err) {
-                let $ = cheerio.load(html);
-                $('#lessons-list').filter(function() {
-                    let data = $(this);
-                    const dataArray = data.children().children().toArray();
-                    const filterData = dataArray.filter(el => el.name === 'link' && el.attribs.itemprop === 'contentUrl');
-                    const filterSpan = dataArray.filter(el => el.name === 'span');
-                    filterSpan.map(el => {
-                        if (el.name === 'span') {
-                            names.push(el.children[0].data);
-                        }
-                    });
-                    filterData.map(el => {
-                        result.push(el.attribs.href);
-                    });
-                    resolve({ result, names });
-                });
-            } else {
-                reject(err);
-            }
-        });
-    });
+	return new Promise(function(resolve, reject) {
+		let result = [];
+		let names = [];
+		request(url, function(err, res, html) {
+			if (!err) {
+				let $ = cheerio.load(html);
+				$('#lessons-list').filter(function() {
+					let data = $(this);
+					const dataArray = data
+						.children()
+						.children()
+						.toArray();
+					const filterData = dataArray.filter(
+						el => el.name === 'link' && el.attribs.itemprop === 'contentUrl'
+					);
+					const filterSpan = dataArray.filter(el => el.name === 'span');
+					filterSpan.map(el => {
+						if (el.name === 'span') {
+							names.push(el.children[0].data);
+						}
+					});
+					filterData.map(el => {
+						result.push(el.attribs.href);
+					});
+					resolve({ result, names });
+				});
+			} else {
+				reject(err);
+			}
+		});
+	});
 }
 
-process.argv.forEach(function (val, index, array) {
-    let videos = [];
-    if (index === 2) {
-        getVideos(val)
-            .then(data => {
-                data.result.map((url, index) => {
-                    videos.push({ url, name: data.names[index] });
-                });
-                console.log('Start download videos, please wait...');
-                videos.map(video => {
-                    console.log(`Start download video: ${video.name}`.blue);
-                    progress(request(video.url), { throttle: 2000, delay: 1000 })
-                        .on('progress', function(state) {})
-                        .on('error', function(err) {
-                            console.log(`${err}`.red);
-                        })
-                        .on('end', function () {
-                            console.log(`End download video ${video.name}`.green);
-                        })
-                        .pipe(fs.createWriteStream(`${video.name}.mp4`));
-                });
-            })
-            .catch(err => console.log(`${err}`.red));
-    }
+process.argv.forEach(function(val, index, array) {
+	let videos = [];
+	if (index === 2) {
+		getVideos(val)
+			.then(data => {
+				data.result.map((url, index) => {
+					videos.push({ url, name: data.names[index] });
+				});
+				console.log('Start download videos, please wait...');
+				videos.map(video => {
+					console.log(`Start download video: ${video.name}`.blue);
+					progress(request(video.url), { throttle: 2000, delay: 1000 })
+						.on('progress', function(state) {})
+						.on('error', function(err) {
+							console.log(`${err}`.red);
+						})
+						.on('end', function() {
+							console.log(`End download video ${video.name}`.green);
+						})
+						.pipe(fs.createWriteStream(`${video.name}.mp4`));
+				});
+			})
+			.catch(err => console.log(`${err}`.red));
+	}
 });
