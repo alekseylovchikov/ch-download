88161da199168b32962a82feb437eca756c3841c
Aleksey Lovchikov <alelov@brabdev.com>
2018-08-26 11:34:35 +0300

    add getToken function

---
+++ src/download/getToken.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+function getToken(e_mail, password) {
+  return new Promise((resolve, reject) => {
+    axios({
+      url: 'https://coursehunters.net/api/auth/login',
+      method: 'put',
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      data: JSON.stringify({ e_mail: e_mail, password: password }),
+    })
+      .then(res => {
+        if (res.data.token) resolve(res.data.token);
+      })
+      .catch(err => reject(err));
+  });
+};
+
+module.exports = getToken;
