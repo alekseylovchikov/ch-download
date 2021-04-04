f4bd8807ada28e5fcdd23be5f749f89e8702f1d4
jivan99 <jivan.katwal99@gmail.com>
2021-04-04 20:16:11 +0545

    fix: fix the logic for grabbing lessons info

--- src/download/getVideos.js
+++ src/download/getVideos.js
@@ -1,55 +1,89 @@
-'use strict';
+"use strict";
 
-const cheerio = require('cheerio');
-let request = require('request');
+const cheerio = require("cheerio");
+let request = require("request");
 request = request.defaults({
-  jar: true
+  jar: true,
 });
 
-const videoMaterialScriptContainerSelector = '.flex-player-item .main-content script:nth-of-type(2)';
-const getLessonsData = $ => {
+const videoMaterialScriptContainerSelector =
+  ".flex-player-item .main-content script:nth-of-type(3)";
+
+const getCourseId = ($) => {
   const lessonsScriptContainer = $(videoMaterialScriptContainerSelector);
-  const lessonsScriptCode = lessonsScriptContainer.get()[0].children[0].data;
-  const coursesObjMatches = lessonsScriptCode.match(/({"title":)(.*)("})/g).slice(1);
 
-  return coursesObjMatches.map(courseString => {
-    const parsedCourseObj = JSON.parse(courseString);
-    return {
-      ...parsedCourseObj,
-      subtitle: parsedCourseObj.subtitle.replace(/(\[)(.*)(])/, '')
-    }
-  });
-}
+  if (lessonsScriptContainer.get().length) {
+    const lessonsScriptCode = lessonsScriptContainer.get()[0].children[0].data;
 
-const courseMaterialsAnchorSelector = '.course-wrap-bottom .ml-15 a:nth-of-type(2)'
-const getCourseMaterialsUrl = $ => {
-  const materialsAnchorEl = $(courseMaterialsAnchorSelector)
+    const courseId = lessonsScriptCode
+      .replace(/\s/g, "")
+      .match(/axios.get\('\/course\/(\d+)\/lessons'\)/g)[0]
+      .match(/\d+/)[0];
+
+    return courseId;
+  } else {
+    return null;
+  }
+};
+
+const courseMaterialsAnchorSelector =
+  ".course-wrap-bottom .ml-15 a:nth-of-type(2)";
+const getCourseMaterialsUrl = ($) => {
+  const materialsAnchorEl = $(courseMaterialsAnchorSelector);
   return materialsAnchorEl.get()[0]?.attribs.href;
-}
+};
 
 function getVideos(url, token) {
   const options = { url };
   if (token) {
     const cookie = request.cookie(token);
     options.headers = {
-      Cookie: cookie
+      Cookie: cookie,
     };
   }
   return new Promise((resolve, reject) => {
-    request(options, function(err, res, html) {
+    request(options, async function (err, res, html) {
       if (!err) {
         let $ = cheerio.load(html);
-        const lessonsData = getLessonsData($)
-        resolve({
-          result: lessonsData.map(lesson => lesson.file),
-          names: lessonsData.map(lesson => lesson.title.replace(/[\/:*?"<>|]/g, '')),
-          urlMaterials: getCourseMaterialsUrl($)
-        })
+        const courseId = getCourseId($);
+
+        if (!courseId) {
+          console.log(
+            "\nPlease check the course url or This course is only available to premium users!"
+              .red
+          );
+          console.log("Try using credentials!".red);
+          return;
+        }
+
+        const options = {
+          url: `https://coursehunter.net/course/${courseId}/lessons`,
+        };
+
+        if (token) {
+          const cookie = request.cookie(token);
+          options.headers = {
+            Cookie: cookie,
+          };
+        }
+
+        request(options, function (err, res) {
+          if (!err) {
+            const lessonsData = JSON.parse(res.body);
+            resolve({
+              result: lessonsData.map((lesson) => lesson.file),
+              names: lessonsData.map((lesson) =>
+                lesson.title.replace(/\s\|\s\d{2}:\d{2}:\d{2}/g, "")
+              ),
+              urlMaterials: getCourseMaterialsUrl($),
+            });
+          }
+        });
       } else {
-        reject(err)
+        reject(err);
       }
-    })
-  })
+    });
+  });
 }
 
 module.exports = getVideos;
