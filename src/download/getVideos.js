40f3d01e92341fb4e63ac4935d0c0fbd131db259
jivan99 <jivan.katwal99@gmail.com>
2021-04-22 17:59:51 +0545

    fix: fix the logic for grabbing lessons name and url

--- src/download/getVideos.js
+++ src/download/getVideos.js
@@ -1,55 +1,99 @@
-'use strict';
+"use strict";
 
-const cheerio = require('cheerio');
-let request = require('request');
+const cheerio = require("cheerio");
+let request = require("request");
 request = request.defaults({
-  jar: true
+  jar: true,
 });
 
-const videoMaterialScriptContainerSelector = '.flex-player-item .main-content script:nth-of-type(2)';
-const getLessonsData = $ => {
-  const lessonsScriptContainer = $(videoMaterialScriptContainerSelector);
-  const lessonsScriptCode = lessonsScriptContainer.get()[0].children[0].data;
-  const coursesObjMatches = lessonsScriptCode.match(/({"title":)(.*)("})/g).slice(1);
+const getCourseId = ($) => {
+  const courseIdScriptSelector =
+    "body.theme_dark .container.pt-20 .comment-form + script";
 
-  return coursesObjMatches.map(courseString => {
-    const parsedCourseObj = JSON.parse(courseString);
-    return {
-      ...parsedCourseObj,
-      subtitle: parsedCourseObj.subtitle.replace(/(\[)(.*)(])/, '')
+  const courseIdScript = $(courseIdScriptSelector);
+
+  if (courseIdScript && courseIdScript.get().length) {
+    const courseIdScriptText = courseIdScript.get()[0].children[0].data;
+
+    const match = courseIdScriptText.match(/\d+/);
+
+    let courseId;
+    if (match) {
+      courseId = match[0];
     }
-  });
-}
 
-const courseMaterialsAnchorSelector = '.course-wrap-bottom .ml-15 a:nth-of-type(2)'
-const getCourseMaterialsUrl = $ => {
-  const materialsAnchorEl = $(courseMaterialsAnchorSelector)
+    return courseId;
+  } else {
+    return null;
+  }
+};
+
+const courseMaterialsAnchorSelector =
+  ".course-wrap-bottom .ml-15 a:nth-of-type(2)";
+const getCourseMaterialsUrl = ($) => {
+  const materialsAnchorEl = $(courseMaterialsAnchorSelector);
   return materialsAnchorEl.get()[0]?.attribs.href;
-}
+};
 
 function getVideos(url, token) {
   const options = { url };
   if (token) {
     const cookie = request.cookie(token);
     options.headers = {
-      Cookie: cookie
+      Cookie: cookie,
     };
   }
   return new Promise((resolve, reject) => {
-    request(options, function(err, res, html) {
+    request(options, async function (err, res, html) {
       if (!err) {
         let $ = cheerio.load(html);
-        const lessonsData = getLessonsData($)
-        resolve({
-          result: lessonsData.map(lesson => lesson.file),
-          names: lessonsData.map(lesson => lesson.title.replace(/[\/:*?"<>|]/g, '')),
-          urlMaterials: getCourseMaterialsUrl($)
-        })
+        const courseId = getCourseId($);
+
+        if (!courseId) {
+          console.log(
+            "\nInvalid course URL or Only available to premium users!".red
+          );
+          console.log("Try using different credentials!".red);
+          return;
+        }
+
+        const options = {
+          url: `https://coursehunter.net/course/${courseId}/lessons`,
+        };
+
+        if (token) {
+          const cookie = request.cookie(token);
+          options.headers = {
+            Cookie: cookie,
+          };
+        }
+
+        request(options, function (err, res) {
+          if (!err) {
+            const lessonsData = JSON.parse(res.body);
+            resolve({
+              result: lessonsData.map((lesson) => lesson.file),
+              names: lessonsData.map((lesson) => {
+                const str = lesson.title.replace(
+                  /\s\|\s\d{2}:\d{2}:\d{2}/g,
+                  ""
+                );
+                const match = str.match(/\d+\.\s.*/g);
+                if (match && match.length) {
+                  return match[0];
+                }
+
+                return str;
+              }),
+              urlMaterials: getCourseMaterialsUrl($),
+            });
+          }
+        });
       } else {
-        reject(err)
+        reject(err);
       }
-    })
-  })
+    });
+  });
 }
 
 module.exports = getVideos;
